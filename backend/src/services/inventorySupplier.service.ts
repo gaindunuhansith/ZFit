fcc040f059a3aa504de8246ec2a1ebabf7fb509b
LehanNawaratne <162142452+LehanNawaratne@users.noreply.github.com>
2025-09-19 13:47:57 +0530

    feat: create supplier service with CRUD methods

---
+++ backend/src/services/inventorySupplier.service.ts
@@ -0,0 +1,37 @@
+import Suppllier from "../models/inventorySupplier.schema.js";
+import type { IInventorySupplier } from "../models/inventorySupplier.schema.js";
+
+export default class SupplierService{
+    //Cretate supplier
+    async createSupplier(data: { supplierName: String, supplierEmail: String, supplierPhone: String, supplierAddress: String}): Promise<IInventorySupplier>{
+        const supplier = new Suppllier(data);
+        return await supplier.save();
+    }
+
+    //Get all suppliers
+    async getAllSupllier(): Promise<IInventorySupplier[]>{
+        return await Suppllier.find().sort({ supplierName: 1})
+    }
+
+    //Get supplier by id
+    async getSupplierById(id: String): Promise<IInventorySupplier| null>{
+        return await Suppllier.findById(id);
+    }
+
+    //Update supplier
+    async updateSupplier(
+        id: String,
+        data: { supplierName: String | undefined, supplierEmail: String | undefined, supplierPhone: String | undefined, supplierAddress: String | undefined}
+    ): Promise<IInventorySupplier | null>{
+        return await Suppllier.findByIdAndUpdate(
+            id,
+            {...data, updatedAt: new Date()},
+            {new: true, runValidators: true}
+        )
+    }
+
+    //Delete supplier
+    async deleteSupllier(id: String): Promise<IInventorySupplier | null>{
+        return await Suppllier.findByIdAndDelete(id);
+    }
+}
