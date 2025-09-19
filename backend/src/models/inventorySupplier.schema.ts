a227b68512a3b152fb8e4644b1fe1a11ae5f0246
LehanNawaratne <162142452+LehanNawaratne@users.noreply.github.com>
2025-09-19 13:12:31 +0530

    feat: create supplier schema

---
+++ backend/src/models/inventorySupplier.schema.ts
@@ -0,0 +1,22 @@
+import mongoose,{ Document } from "mongoose";
+
+export interface IInventorySupplier extends Document {
+    supplierName: string;
+    supplierEmail: string;
+    supplierPhone: string;
+    supplierAddress: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const SupplierSchema = new mongoose.Schema<IInventorySupplier>({
+    supplierName: {type: String, required: true},
+    supplierEmail: {type: String, required: true},
+    supplierPhone: {type: String, required: true},
+    supplierAddress: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: {type: Date, default: Date.now}
+})
+
+const Suppllier = mongoose.model<IInventorySupplier>("Supplier", SupplierSchema)
+export default Suppllier;
