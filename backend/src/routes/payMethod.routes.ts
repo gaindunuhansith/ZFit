f92b4fe75a866a0967aba956ec0d32c8a8cffc73
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2025-09-20 14:22:08 +0530

    fix(payMethod): edit some codes in paymethod route

--- backend/src/routes/payMethod.routes.ts
+++ backend/src/routes/payMethod.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
 import {
-    createPaymentMethod,
-    getPaymentMethods,
-    getPaymentMethodById,
-    updatePaymentMethod,
-    deletePaymentMethod
+    createPaymentMethod, getPaymentMethods, getPaymentMethodById, updatePaymentMethod, deletePaymentMethod
 } from '../controllers/payMethod.controller.js';
+//middleware
 
 const router = Router();
 
-// Payment method routes
-router.get('/', getPaymentMethods);
-router.get('/:id', getPaymentMethodById);
-router.post('/', createPaymentMethod);
+//router.get('/',  getPaymentMethods);
+router.get('/:id',getPaymentMethodById);
+//router.post('/',  createPaymentMethod);
 router.put('/:id', updatePaymentMethod);
-router.delete('/:id', deletePaymentMethod);
+router.delete('/:id',deletePaymentMethod);
 
 export default router;
