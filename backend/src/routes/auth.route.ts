f123f496a0aecb2ba174c77f7692bd5750075d76
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-09-23 00:05:26 +0530

    feat: fully implement the refresh token flow with correct flows

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
     loginHandler,
     logoutHandler,
+    refreshTokenHandler,
     registerHandler,
     verifyEmailHandler,
 } from "../controllers/auth.controller.js";
@@ -10,6 +11,7 @@ const authRouter = Router();
 
 authRouter.post("/register", registerHandler);
 authRouter.post("/login", loginHandler);
+authRouter.get("/refresh", refreshTokenHandler);
 authRouter.get("/logout", logoutHandler);
 authRouter.get("/email/verify/:code", verifyEmailHandler);
 
