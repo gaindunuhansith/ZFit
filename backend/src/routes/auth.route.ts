620938bc2cdcae36034524776425a247aa87f827
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-09-22 21:38:53 +0530

    feat: implement the login routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {
+    loginHandler,
     registerHandler,
 } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", registerHandler);
+authRouter.post("/login", loginHandler)
 
 export default authRouter;
