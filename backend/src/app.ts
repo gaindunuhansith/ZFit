0cff345897f7f68e05e1ce91c338db683ac90504
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2025-09-23 18:15:13 +0530

    fix(report&analytics): add routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,6 +14,7 @@ import invoiceRoutes from "./routes/invoice.routes.js";  // Added invoice routes
 import paymentRoutes from "./routes/payment.routes.js";  // Added payment routes
 import payMethodRoutes from "./routes/payMethod.routes.js";  // Added payMethod routes
 import refundRoutes from "./routes/refund.routes.js";  // Added refund routes
+import reportPaymentRoutes from "./routes/reportPayment.routes.js";  // Added report payment routes
 import cartRoutes from "./routes/cart.routes.js";
 import orderRouter from "./routes/order.routes.js"
 
@@ -41,6 +42,7 @@ app.use('/api/v1/invoices', invoiceRoutes);  // Added invoice routes
 app.use('/api/v1/payments', paymentRoutes);  // Added payment routes
 app.use('/api/v1/paymethods', payMethodRoutes);  // Added payMethod routes
 app.use('/api/v1/refunds', refundRoutes);  // Added refund routes
+app.use('/api/v1/reports', reportPaymentRoutes);  // Added report payment routes
 
 //API health check
 app.get("/", (req: express.Request, res: express.Response) => {  // Changed from app.use to app.get
