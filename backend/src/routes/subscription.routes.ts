226b3ee20afb16b50e9f4e60ea6d0de7b16dfdde
Gaindu Nuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-09-26 03:20:12 +0530

    fix: fix the unnecessary comment

--- backend/src/routes/subscription.routes.ts
+++ backend/src/routes/subscription.routes.ts
@@ -17,7 +17,6 @@ import {
 
 const subscriptionRouter = Router();
 
-// GET routes
 subscriptionRouter.get("/", getAllSubscriptionsHandler);
 subscriptionRouter.get("/active", getActiveSubscriptionsHandler);
 subscriptionRouter.get("/expiring", getExpiringSubscriptionsHandler);
@@ -25,19 +24,15 @@ subscriptionRouter.get("/user/:userId", getUserSubscriptionsHandler);
 subscriptionRouter.get("/user/:userId/active", getUserActiveSubscriptionHandler);
 subscriptionRouter.get("/:id", getSubscriptionByIdHandler);
 
-// POST routes
 subscriptionRouter.post("/", createSubscriptionHandler);
 
-// PUT routes
 subscriptionRouter.put("/:id", updateSubscriptionHandler);
 
-// PATCH routes
 subscriptionRouter.patch("/:id/cancel", cancelSubscriptionHandler);
 subscriptionRouter.patch("/:id/pause", pauseSubscriptionHandler);
 subscriptionRouter.patch("/:id/resume", resumeSubscriptionHandler);
 subscriptionRouter.patch("/:id/extend", extendSubscriptionHandler);
 
-// DELETE routes
 subscriptionRouter.delete("/:id", deleteSubscriptionHandler);
 
 export default subscriptionRouter;
