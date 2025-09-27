ef25f25915eee6885aac4615961cf65120a704bd
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2025-09-27 18:30:19 +0530

    feat:complete invoice crud

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,7 @@ import inventoryRoutes from "./routes/inventory.routes.js";
 import paymentRoutes from "./routes/payment.routes.js";  
 import BookingRoutes from "./routes/Booking.routes.js";
 import progressRoutes from "./routes/progress.routes.js";
+import invoiceRoutes from "./routes/invoice.routes.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 
 //creating a express app instance
@@ -36,6 +37,7 @@ app.use('/api/v1/inventory', inventoryRoutes);
 app.use('/api/v1/payments', paymentRoutes);  
 app.use('/api/v1/bookings', BookingRoutes);
 app.use('/api/v1/progress', progressRoutes);
+app.use('/api/v1/invoices', invoiceRoutes);
 
 app.use(errorMiddleware);
 
