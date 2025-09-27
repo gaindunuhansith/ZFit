ce299d1b14b4b6028f59b8d7bc852cb7219cfe1d
K.B.Pavith Kavisika <kbpkavisika@gmail.com>
2025-09-27 14:09:09 +0530

    fix: fix refund errors

--- backend/src/routes/refund.routes.ts
+++ backend/src/routes/refund.routes.ts
@@ -6,10 +6,10 @@ import authenticate from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.get('/', authenticate(["manager","staff"]), getRefunds);
-router.get('/:id', authenticate(["manager","staff"]), getRefundById);
-router.post('/', authenticate(["manager","staff"]), createRefund);
-router.put('/:id', authenticate(["manager","staff"]), updateRefund);
-router.delete('/:id', authenticate(["manager","staff"]), deleteRefund);
+router.get('/', getRefunds);
+router.get('/:id', getRefundById);
+router.post('/',  createRefund);
+router.put('/:id', updateRefund);
+router.delete('/:id', deleteRefund);
 
 export default router;
